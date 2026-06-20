1fc9555aec2c29e46c5ee0e2db3c6951a8fb4b52
Mina Liang <mliang3@academic.rrc.ca>
2026-06-20 17:24:55 -0500

    fix: insert a 'module' type in the script tag in 'event_signup.html to load the script as a module

--- event_signup/event_signup.js
+++ event_signup/event_signup.js
@@ -1,7 +1,8 @@
-import "../template.js";
+import "../template.js"
 
 // get the form element
 const form = document.getElementById("event-signup-form");
+const formStatus = document.getElementById("form-status");
 
 // a function to validate the Event Name
 function isValidEventName(eventName) {
@@ -26,23 +27,23 @@ function isValidRole(role) {
     return role.length > 0;
 }
 
+// a function that collect the form values
+function formValues() {
+    const formValues = {
+        eventName: document.getElementById("event-name").value,
+        repName: document.getElementById("rep-name").value,
+        repEmail: document.getElementById("rep-email").value,
+        role: document.getElementById("role-selection").value
+    };
+    return formValues;
+}
+
 // a function to collect form value
 function clearErrors() {
     const errorMessages = document.querySelectorAll(".error-message");
     errorMessages.forEach(message => {
         message.computedStyleMap.display = "none";
     });
-}
-
-// a function that collect the form values
-function formValues() {
-    const formValues = {
-        eventName = document.getElementById("event-name").value,
-        repName = document.getElementById("rep-name").value,
-        repEmail = document.getElementById("rep-email").value,
-        role = document.getElementById("role-selection").value
-    };
-    return formValues;
 }
 
 // a function to display error for a specific field
@@ -58,28 +59,46 @@ function showError(fieldId) {
 // form submission handler
 form.addEventListener("submit", function(event) {
     event.preventDefault();
-})
+
+    clearErrors();
+
+    formValues();
+
+    let isValid = true;
 
 // validate event name
-if (!validateEventName(formValues.eventName)) {
+if (!isValidEventName(formValues.eventName)) {
     showError("group-event-name");
     isValidEventName = false;
 }
 
 // validate representative name
-if (!validateRepName(formValues.repName)) {
+if (!isValidRepName(formValues.repName)) {
     showError("group-rep-name");
     isValidRepName = false;
 }
 
 // validate email 
-if (!validateEmail(formValues.repEmail)) {
+if (!isValidEmail(formValues.repEmail)) {
     showError("group-rep-email");
     isValidEmail = false;
 }
 
 // validate role selection
-if (!validateRole(formValues.role)) {
+if (!isValidRole(formValues.role)) {
     showError("group-role");
-    isValidEmail = false;
+    isValidRole = false;
 }
+
+if (isValid) {
+    // display success message
+    formStatus.textContent = "Form submitted successfully!";
+    formStatus.style.color = "#2e7d32";
+
+    // console.log("Form value are collected:", formValue);
+} else {
+    // display error message
+    formStatus.textContent = "Please coorect the errors above";
+    formStatus.style.color = "#c0392b"
+}
+});
