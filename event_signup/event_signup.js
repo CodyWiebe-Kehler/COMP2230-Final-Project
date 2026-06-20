6ce799cda3f0dc267e8c95536f94717a3d996ac1
Mina Liang <mliang3@academic.rrc.ca>
2026-06-20 15:58:17 -0500

    feat: implement a 'clearErrors' and 'showError' functions to clear errors and show error if the validation fails

--- event_signup/event_signup.js
+++ event_signup/event_signup.js
@@ -26,6 +26,14 @@ function isValidRole(role) {
     return role.length > 0;
 }
 
+// a function to collect form value
+function clearErrors() {
+    const errorMessages = document.querySelectorAll(".error-message");
+    errorMessages.forEach(message => {
+        message.computedStyleMap.display = "none";
+    });
+}
+
 // a function that collect the form values
 function formValues() {
     const formValues = {
@@ -37,6 +45,41 @@ function formValues() {
     return formValues;
 }
 
+// a function to display error for a specific field
+function showError(fieldId) {
+    const formGroup = document.querySelectorAll(".error-message");
+    const errorMessages = formGroup.querySelector(".error-message");
+    if (errorMessages) {
+        errorMessages.style.display = "block";
+    }
+}
+
+
+// form submission handler
 form.addEventListener("submit", function(event) {
     event.preventDefault();
 })
+
+// validate event name
+if (!validateEventName(formValues.eventName)) {
+    showError("group-event-name");
+    isValidEventName = false;
+}
+
+// validate representative name
+if (!validateRepName(formValues.repName)) {
+    showError("group-rep-name");
+    isValidRepName = false;
+}
+
+// validate email 
+if (!validateEmail(formValues.repEmail)) {
+    showError("group-rep-email");
+    isValidEmail = false;
+}
+
+// validate role selection
+if (!validateRole(formValues.role)) {
+    showError("group-role");
+    isValidEmail = false;
+}
