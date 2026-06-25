2185957dab2030bd3ee611807fec5ada76db2fe8
kavya merai <kmerai@rrc.ca>
2026-06-25 02:28:50 -0500

    Changes made to the css file to make the dark-mode feature work.

--- volunteer_tracker/volunteer_tracker.js
+++ volunteer_tracker/volunteer_tracker.js
@@ -186,3 +186,15 @@ if (typeof module !== "undefined") {
     };
 
 }
+
+const themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")){
+        themeBtn.textContent = "☀️ Light Mode";
+    }else{
+        themeBtn.textContent = "🌙 Dark Mode";
+    }
+});
